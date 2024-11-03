fa0f31025eb8772f11c13d11e1b4cf9cf0eb7206
Vincent Van Potent <harshithakash945@gmail.com>
2024-11-02 22:07:12 -0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,8 @@
+local placeId = game.PlaceId
+if placeId == 14702923685 or placeId == 16335600896 or placeId ==16335600116 then
+    loadstring(game:HttpGet("The main thing for rr"))()
+elseif placeId == 13950108612 then
+    loadstring(game:HttpGet("the lobby"))()
+else
+    print("Game Not Supported :(")
+end
