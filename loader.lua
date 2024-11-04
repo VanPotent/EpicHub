51cb3385fb8233c2108c02fe5a4b52e06603cca2
Vincent Van Potent <harshithakash945@gmail.com>
2024-11-03 20:36:55 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 local placeId = game.PlaceId
 if placeId == 14702923685 or placeId == 16335600896 or placeId ==16335600116 then --radiant main
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/rad"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/rad-main"))()
 elseif placeId == 13950108612 then --radiant lobby
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/rad-lob"))()
 else
