c5f0e2504b4354470a16a3de0ffc77cb0c5cf67f
Vincent Van Potent <harshithakash945@gmail.com>
2024-11-11 14:20:39 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,6 +3,10 @@ if placeId == 14702923685 or placeId == 16335600896 or placeId ==16335600116 the
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/rad-main"))()
 elseif placeId == 13950108612 then --radiant lobby
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/rad-lob"))()
+
+elseif placeId == 7449423635 or placeId == 4442272183 or placeId == 2753915549 then --Blox fruits seas in order
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/bf-chest"))()
+
 else
     print("Game Not Supported :(")
 end
