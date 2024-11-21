e5dd76fafb9b58cd5a70c828f02e91944b26996d
Vincent Van Potent <harshithakash945@gmail.com>
2024-11-21 12:30:48 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,5 @@
 local placeId = game.PlaceId
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/wh.lua"))()
 if placeId == 14702923685 or placeId == 16335600896 or placeId ==16335600116 then --radiant main
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/rad-main"))()
 elseif placeId == 13950108612 then --radiant lobby
@@ -8,7 +9,6 @@ elseif placeId == 7449423635 or placeId == 4442272183 or placeId == 2753915549 t
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/bf-chest"))()
 elseif placeId == 128743078333456 then --wacky worlds
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/ww-main"))()
-    
 
 else
     print("Game Not Supported :(")
