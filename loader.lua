968b35b661951a795007f1173708e9f45965eeb1
Vincent Van Potent <harshithakash945@gmail.com>
2024-11-29 09:45:51 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,7 +6,7 @@ elseif placeId == 13950108612 then --radiant lobby
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/rad-lob"))()
 
 elseif placeId == 7449423635 or placeId == 4442272183 or placeId == 2753915549 then --Blox fruits seas in order
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/bf-chest"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/BFGUI"))()
 elseif placeId == 128743078333456 then --wacky worlds
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VanPotent/EpicHub/refs/heads/main/ww-main"))()
 elseif placeId == 91282350711571 then  -- mad city chapter 1
