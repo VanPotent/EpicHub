af03b417f9796c9819b7fa4819fe7fd5d78c5aa0
Vincent Van Potent <harshithakash945@gmail.com>
2025-04-10 10:16:55 -0700

    Update orion.lua

--- orion.lua
+++ orion.lua
@@ -1,5 +1,4 @@
-
-
+-- not made by me, using this for archive purposes. all rights go to original author
 local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
 local RunService = game:GetService("RunService")
